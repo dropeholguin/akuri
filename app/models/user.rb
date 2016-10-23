8b64bd9cf6a84aba4bea4c39a0ae61d2817162d9
Pedro Holguin <dropeholguin1994@gmail.com>
2016-10-23 13:41:34 -0500

    Fixed home page, register page, log in and edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default-avatar.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  def full_name
+    [firts_name, last_name].join(' ')
+  end
 end
