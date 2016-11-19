b5cd1d54f6cc70d300bf0e0c8ea3345f9d83a34f
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2016-11-19 18:34:33 -0500

    Se actualiza controlador de stablisments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -61,6 +61,8 @@ class CommentsController < ApplicationController
     end
   end
 
+  #Use delete
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
