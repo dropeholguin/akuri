7a4a777405cec36739764c6ea0de18db01024a43
Pedro <dropeholguin1994@gmail.com>
2016-11-21 01:22:14 -0500

    Create file seed

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -6,7 +6,7 @@ class Establishment < ApplicationRecord
   has_many :locations
   
   validates :name, :kind_of_food, :phone, :description, :image, presence: true
-  validates :description, length: { maximum: 50 }
+  validates :description, length: { maximum: 100 }
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
